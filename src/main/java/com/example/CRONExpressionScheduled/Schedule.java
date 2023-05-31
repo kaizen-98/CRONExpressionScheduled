3141e4bf4c87a6f6f207dec13f94a91805de10a3
kaizen-98 <ammalnassri@gmail.com>
2023-05-31 15:02:03 +0400

    problem8

--- src/main/java/com/example/CRONExpressionScheduled/Schedule.java
+++ src/main/java/com/example/CRONExpressionScheduled/Schedule.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Schedule {
-    @Scheduled(cron = "0 */15 8-22 * * *")
+    @Scheduled(cron = "0 0 9-17 * * 1-5")
     public void schedule(){
         System.out.println("Hello world!");
     }
